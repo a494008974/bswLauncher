68e74f7aae8ef28528c71f2d22c33601b2584e91
zhou <494008974@qq.com>
2018-11-07 18:41:48 +0800

    zhou3

--- module-zhou/src/main/java/com/mylove/zhou/MainActivity.java
+++ module-zhou/src/main/java/com/mylove/zhou/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import com.alibaba.android.arouter.facade.annotation.Route;
 
 import me.jessyan.armscomponent.commonsdk.core.RouterHub;
+import me.jessyan.armscomponent.commonservice.dao.User;
 import me.jessyan.armscomponent.commonservice.zhou.dao.ZhouDao;
 
 @Route(path = RouterHub.ZHOU_HOMEACTIVITY)
@@ -20,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
         User user = new User();
         user.setUsername("zhou");
         user.setPassword("1234aaaaa");
-        user.setEmail("494008974@qq.com");
         ZhouDao.insert(user);
     }
 }
